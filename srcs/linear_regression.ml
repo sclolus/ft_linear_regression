1b45c5dcba1303b9bc55d85ed13d960313f0a3ab
sclolus <sclolus@student.42.fr>
2018-10-19 03:06:54 +0200

    Well atleast it's converging to something. But is the direction coefficient at the perpendicular to the data

--- srcs/linear_regression.ml
+++ srcs/linear_regression.ml
@@ -6,7 +6,7 @@
 (*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2018/10/17 06:24:19 by sclolus           #+#    #+#             *)
-(*   Updated: 2018/10/17 09:09:49 by sclolus          ###   ########.fr       *)
+(*   Updated: 2018/10/19 03:00:09 by sclolus          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -26,21 +26,26 @@ let take lst size =
                                        
 let train_on_dataset dataset learning_rate =
   let data_length = List.length dataset in
+  let prefix_average_factor = (1.0 /. (float_of_int data_length)) in
   let rec aux theta0 theta1 current_size =
-    if current_size > data_length
+    if current_size > (* data_length * data_length * data_length *) 100000
     then (theta0, theta1)
     else
       begin
-        let prefix_factors = learning_rate *. (1.0 /. float_of_int data_length) in
-        let folding_closure = (fun acc (mileage, price) -> acc +. ((guess_target mileage theta0 theta1) -. price)) in
-        let tmp_theta0 = (prefix_factors *. List.fold_left folding_closure 0.0 dataset) in
-        let folding_closure = (fun acc (mileage, price) -> acc +. (((guess_target mileage theta0 theta1) -. price) *. mileage)) in
-        let tmp_theta1 = (prefix_factors *. List.fold_left folding_closure 0.0 dataset) in
-        Printf.printf "t0 = %f, t1 = %f\n" tmp_theta0 tmp_theta1 ;
-        (* let (mileage, price) = List.nth dataset (current_size - 1) in
-         * let tmp_theta0 = theta0 -. (prefix_factors *. ((guess_target mileage theta0 theta1) -. price)) in
-         * let tmp_theta1 = theta1 -. (prefix_factors *. (((guess_target mileage theta0 theta1) -. price) *. mileage)) in *)
-        aux tmp_theta0 tmp_theta1 (current_size + 1)
+        let error_closure = (fun (feature, result) -> guess_target feature theta0 theta1 -. result) in
+        let error_squared_closure = (fun (feature, result) -> (error_closure (feature, result)) ** 2.0) in
+        Printf.printf "actual residual error squared: %f -< %f\n" (List.fold_left (fun acc (feature, result) -> acc +. error_squared_closure (feature, result)) 0.0 dataset)
+        (List.fold_left (fun acc (feature, result) -> acc +. error_closure (feature, result)) 0.0 dataset);
+        let error_sum = (fun acc (feature, result) -> acc +. error_closure (feature, result)) in
+        let theta1_derivative_sum = (fun acc (feature, result) -> acc +. (error_closure (feature, result)) *. feature) in
+        let theta0_derivative = learning_rate *. prefix_average_factor *. List.fold_left error_sum 0.0 dataset in
+        let theta1_derivative = learning_rate *. prefix_average_factor *. List.fold_left theta1_derivative_sum 0.0 dataset in
+        (* Printf.printf "t0_df = %.15f\nt1_df = %.15f\n" theta0_derivative theta1_derivative ;
+         * Printf.printf "t0 = %.15f, t1 = %.15f\n" theta0 theta1 ; *)
+
+        let new_theta0 = theta0 +. theta0_derivative in
+        let new_theta1 = theta1 -. theta1_derivative in
+        aux new_theta0 new_theta1 (current_size + 1)
       end
   in
-  aux 0.0 0.0 1    
+  aux 0.0 0.0 0
--- srcs/train.ml
+++ srcs/train.ml
@@ -6,7 +6,7 @@
 (*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2018/10/17 05:50:22 by sclolus           #+#    #+#             *)
-(*   Updated: 2018/10/17 08:54:49 by sclolus          ###   ########.fr       *)
+(*   Updated: 2018/10/19 03:00:38 by sclolus          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -50,6 +50,8 @@ let () =
     let dataset = Parsing.parse_dataset in_channel in
     Dataset.print_dataset dataset ;
     print_endline "Now training on dataset" ;
-    let (t0, t1) = Linear_regression.train_on_dataset dataset 0.0001 in
+    let (t0, t1) = Linear_regression.train_on_dataset dataset 1e-12  in
     Printf.printf "Finished training: t0 = %f, t1 = %f\n" t0 t1 ;
-    Printf.printf "guess on 24000:3650 -> %f" (Linear_regression.guess_target 74000.0 t0 t1) ;
+    List.iter (fun (mileage, price) -> let prediction = (Linear_regression.guess_target mileage t0 t1) in
+                                       Printf.printf "guess on %f:%f -> %f\n" mileage price prediction) dataset
+
